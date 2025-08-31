1b69cdaceb061956cac2dd0a5b8f1b25e058f460
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-08-31 12:19:36 +0530

    31/08/2025

--- Chapter3/string.py
+++ Chapter3/string.py
@@ -1,4 +1,26 @@
- a='Anand'
- b="Anand"
- c='''Anand'''
+name='Anand'
  
+ 
+nameShort=name[0:3]# start from index 0 to index 2. first index is include and last index 3 is not included
+print(nameShort)
+ 
+character=name[1] # first character of the string
+print(character)    
+
+# starting se index 0 se start hota hai
+# ending se index -1 se start hota hai
+print(name[-4: -1])
+print(name[1:4])# corresponding positive index is 1 to 3
+
+print(name[:4]) # start from index 0 to index 3
+print(name[1:]) # start from index 1 to last index
+
+
+# slicing with skip value
+b="abcdefghijklmnopqrstuvwxyz"
+print(b[1:9:4])# output is bf 
+
+# other advanced slicing techniques
+word="amazing"
+word=[:7] # same as word[0:7]
+word=[0:]# same as word[0:len(word)]
